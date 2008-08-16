42c31a894c5ed246b4981a335dbbfbfc05adfc2e
luv2code <luv2code@07e4d8ed-c153-0410-a65f-6bbd032d9f65>
2008-08-16 04:10:38 +0000

    Started on the feed reader portion of the app.

---
+++ PodcastConverter/FeedStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PodcastConverter
+{
+    [Serializable]
+    public class FeedStore
+    {
+        public List<Feed> Feeds { get; set; }
+    }
+
+    [Serializable]
+    public class Feed
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
